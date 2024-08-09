96d7230484f1822eee7e75ae5fcdcd0bda97012d
François Werbrouck <122839953+Francois-Werbrouck@users.noreply.github.com>
2024-08-09 17:06:29 +0000

    Issue #96: Refactoring new_inspection

--- datastore/fertiscan/__init__.py
+++ datastore/fertiscan/__init__.py
@@ -26,7 +26,11 @@ if FERTISCAN_STORAGE_URL is None or FERTISCAN_STORAGE_URL == "":
 
 
 async def register_analysis(
-    cursor, container_client, user_id, hashed_pictures, analysis_dict,
+    cursor,
+    container_client,
+    user_id,
+    hashed_pictures,
+    analysis_dict,
 ):
     """
     Register an analysis in the database
@@ -49,18 +53,30 @@ async def register_analysis(
             raise datastore.ContainerCreationError(
                 f"Container not found based on the given user_id: {user_id}"
             )
-        
-        #Create picture set for this analysis
-        picture_set_metadata= data_picture_set.build_picture_set(user_id, len(hashed_pictures))
-        picture_set_id = picture.new_picture_set(cursor,picture_set_metadata, user_id,"General")
 
-        #Upload pictures to storage
-        await datastore.upload_pictures(cursor=cursor,user_id=user_id, container_client=container_client, picture_set_id=picture_set_id, hashed_pictures=hashed_pictures)
-        
-        #Register analysis in the database
+        # Create picture set for this analysis
+        picture_set_metadata = data_picture_set.build_picture_set(
+            user_id, len(hashed_pictures)
+        )
+        picture_set_id = picture.new_picture_set(
+            cursor, picture_set_metadata, user_id, "General"
+        )
+
+        # Upload pictures to storage
+        await datastore.upload_pictures(
+            cursor=cursor,
+            user_id=user_id,
+            container_client=container_client,
+            picture_set_id=picture_set_id,
+            hashed_pictures=hashed_pictures,
+        )
+
+        # Register analysis in the database
         formatted_analysis = data_inspection.build_inspection_import(analysis_dict)
-        
-        analysis_db = inspection.new_inspection_with_label_info(cursor, user_id, picture_set_id, formatted_analysis)
+
+        analysis_db = inspection.new_inspection_with_label_info(
+            cursor, user_id, picture_set_id, formatted_analysis
+        )
         return analysis_db
     except inspection.InspectionCreationError:
         raise Exception("Datastore Inspection Creation Error")
@@ -70,7 +86,85 @@ async def register_analysis(
         print(e.__str__())
         raise Exception("Datastore unhandeled error")
 
-async def get_user_unverified_analysis(cursor,user_id):
+
+async def get_full_inspection_json(
+    cursor,
+    inspection_id,
+    user_id=None,
+    picture_set_id=None,
+    label_info_id=None,
+    company_info_id=None,
+    manufacturer_info_id=None,
+):
+    """
+    Get the full inspection json from the database
+
+    Parameters:
+    - cursor: The cursor object to interact with the database.
+    - inspection_id: The inspection id of the inspection.
+
+    Returns:
+    - The inspection json.
+    """
+    try:
+        if not inspection.is_a_inspection_id(
+            cursor=cursor, inspection_id=inspection_id
+        ):
+            raise inspection.InspectionNotFoundError(
+                f"Inspection not found based on the given id: {inspection_id}"
+            )
+
+        # Check Ids
+        if (
+            (picture_set_id is None or picture_set_id == "" or not picture.is_a_picture_set_id(cursor=cursor, picture_set_id=picture_set_id))
+            or (user_id is None or user_id == "" or not user.is_a_user_id(cursor=cursor, user_id=user_id))
+            or (label_info_id is None or label_info_id == "")
+            or (company_info_id is None or company_info_id == "")
+            or (manufacturer_info_id is None or manufacturer_info_id == "")
+        ):
+            ids = inspection.get_inspection_fk(cursor, inspection_id)
+            picture_set_id = ids[2]
+            label_info_id = ids[0]
+            company_info_id = ids[3]
+            manufacturer_info_id = ids[4]
+            user_id = ids[1]
+        else:
+            if not picture.is_a_picture_set_id(cursor=cursor, picture_set_id=picture_set_id):
+                raise picture.PictureSetNotFoundError(
+                    f"Picture set not found based on the given id: {picture_set_id}"
+                )
+            if not user.is_a_user_id(cursor=cursor, user_id=user_id):
+                raise user.UserNotFoundError(
+                    f"User not found based on the given id: {user_id}"
+                )
+            ids = inspection.get_inspection_fk(cursor, inspection_id)
+            if not picture_set_id == ids[2]:
+                raise Exception("Picture set id does not match the picture_set_id in the inspection for the given inspection_id")
+            if not label_info_id == ids[0]:
+                raise Exception("Label info id does not match the label_info_id in the inspection for the given inspection_id")
+            if not company_info_id == ids[3]:
+                raise Exception("Company info id does not match the company_info_id in the inspection for the given inspection_id")
+            if not manufacturer_info_id == ids[4]:
+                raise Exception("Manufacturer info id does not match the manufacturer_info_id in the inspection for the given inspection_id")
+            if not user_id == ids[1]:
+                raise Exception("User id does not match the user_id in the inspection for the given inspection_id")
+        
+        # Retrieve pictures
+        pictures_ids = picture.get_picture_in_picture_set(cursor, picture_set_id)
+
+        # Retrieve label_info
+        empty = data_inspection.Inspection()
+        label_metadata=data_inspection.build_inspection_export(cursor,inspection_id, label_info_id, company_info_id, manufacturer_info_id)
+
+        return inspection.get_inspection(cursor, inspection_id)
+    except inspection.InspectionNotFoundError:
+        raise
+    except Exception as e:
+        print(e.__str__())
+        raise Exception("Datastore unhandeled error")
+
+
+async def get_user_unverified_analysis(cursor, user_id):
     """
     This function fetch all the unverified inspection of a user
 
@@ -90,6 +184,8 @@ async def get_user_unverified_analysis(cursor,user_id):
         label_info.product_name,
         label_info.company_info_id,
         label_info.manufacturer_info_id
+        company_info.id as company_info_id,
+        company_info.company_name
     ]
     """
 
@@ -98,7 +194,7 @@ async def get_user_unverified_analysis(cursor,user_id):
             raise user.UserNotFoundError(
                 f"User not found based on the given id: {user_id}"
             )
-        return inspection.get_all_user_unverified_inspection(cursor, user_id)
+        return inspection.get_all_user_inspection_filter_verified(cursor, user_id,False)
     except user.UserNotFoundError:
         raise
     except Exception as e:
